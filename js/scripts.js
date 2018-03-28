466ae15a8115b4adb11351b2fe298dd8ef60fb4e
julia sheremet and matt nardoni <julia@>
2018-03-28 16:32:53 -0700

    add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
-//user interface
-// $(document).ready(function(){
-//   $("form#translator-form").submit(function(event){
-//     event.preventDefault();
-//
-//     var userInput = $("#userInput").val();
+// user interface
+$(document).ready(function(){
+  $("form#translator-form").submit(function(event){
+    event.preventDefault();
+
+    var userInput = $("#userInput").val();
+    var result = pigLatin(userInput);
 //     // var characters = userInput.match(/^[a-zA-Z]+$/g);
 //     // var vowels = ('aeiouy');
 //     // var vowelsSplit = vowels.split('');
 //     // var firstLetter = userInput.charAt(0);
 //     // var findVowels = userInput.match(/[aeiou]/gi);
 //     // var splitUserInput = userInput.split('');
-//
-//     // debugger;
+
+    $("#result").text(result);
+
+
 //
 //     if (vowelsSplit[0 || 1 || 2 || 3 || 4 || 5] === firstLetter[0]) {
 //      $("#result").append(userInput + "way"); //true
@@ -40,46 +43,39 @@
 //     //   $("#result").append("ay"); //true
 //     // }
 //
-//   });
-// });
+  });
+});
 
 //backend logic
 var arrayVowels = ['a', 'e', 'i', 'o', 'u'];
 var arrayConsonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
-//
-// var pigLatin = function(userInput) {
-//   if (userInput === "") {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
+// var userInput = "able"
 function pigLatin (userInput){
   for (var i = 0; i < arrayVowels.length; i++) {
     if (userInput === arrayVowels[i] || (userInput[i] === arrayVowels[i])){
       return userInput + 'way';
     }
   }
-  for (var i = 0; i < arrayConsonants.length; i++) {
-    if (userInput === arrayConsonants[i] || (userInput[i] === arrayConsonants[i])) {
+  for (var k = 0; k < arrayConsonants.length; k++) {
+    if (userInput === arrayConsonants[k] || (userInput[k] === arrayConsonants[k])) {
       return userInput + 'ay';
     }
   }
-  var userInputArray = userInput.split(" "); {
+  var userInputArray = userInput.split("");
     return userInputArray;
-  }
-    // userInput.array[i].charAt[i];
-//     for (var i=0; i<userInputArray.length;i++)
 }
 
+  // for (var m=0; m<arrayConsonants.length; m++) {
+  //   userInput.
 
+  // var userInputArray = userInput.split('');
+    // userInputArray.forEach(function(userArray) {
+      // return userInputArray;
+      // debugger;
+    // });
+    // (var i=0; i<userInputArray.length;i++) {
+    //  if (userInputArray.array[i])
 
+  // } (userInputArray.array[i].charAt[i])
 
-// var leapYear = function(year) {
-//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+//     (userInputArray.array[i].charAt[i])
